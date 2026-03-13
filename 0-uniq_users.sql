6f5a0700cd28f1dcbaa2993bc7bda325136cfaff
Elnara Malikzade <12728@holbertonstudents.com>
2026-03-13 18:39:09 +0000

    Delete redundant file

--- 0-uniq_users.sql
+++
@@ -1,6 +0,0 @@
--- A SQL script that creates a table users
-CREATE TABLE IF NOT EXISTS users (
-       id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-       email VARCHAR(255) UNIQUE NOT NULL,
-       name VARCHAR(255) NOT NULL
-);
