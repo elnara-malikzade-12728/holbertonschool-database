776e6edf76905b682994ba879169641b5c1e528a
Elnara Malikzade <12728@holbertonstudents.com>
2026-03-14 08:32:15 +0000

    Add 3-glam_rock.sql

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- a SQL script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, (split - formed) AS lifespan
+FROM metal_bands
+ORDER BY lifespan DESC;
