9bbd158ec1c3868b62a45880c099e5f412adadc9
Elnara Malikzade <12728@holbertonstudents.com>
2026-03-20 13:59:39 +0000

    Add exercise.py main3.py for task3

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -14,7 +14,18 @@ def count_calls(method: Callable) -> Callable:
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
-                
+
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(inputs_key, str(args))
+        result =  method(self, *args, **kwargs)
+        self._redis.lpush(outputs_key, result)
+        return result
+    return wrapper
+
 class Cache:
     """
     A class with __init__ method and a private variable
@@ -28,6 +39,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
@@ -60,3 +72,6 @@ class Cache:
         """
         key = self.get(key, int)
         return key
+
+
+    
