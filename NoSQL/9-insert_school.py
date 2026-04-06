873b1a1ab77d439f9d049cfeb4e43694539dc210
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-06 09:34:45 +0000

    Add 9-insert_school.py

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+// a Python function insert_school
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    a Python function that inserts a new document
+    in a collection based on kwargs and returns _id
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
