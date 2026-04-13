3f2e19c5b7ca2e21d58ede4f00a0f70eb9be6c33
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-13 10:40:34 +0000

    Modify exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module has a Cache class with __init__ method and a private variable 
+This module has a Cache class with __init__ method and a private variable
 """
 import uuid
 from typing import Union, Callable, Optional
@@ -21,6 +21,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     """
     Uses functional wrapper to call history
@@ -33,21 +34,39 @@ def call_history(method: Callable) -> Callable:
         inputs_key = f"{method.__qualname__}:inputs"
         outputs_key = f"{method.__qualname__}:outputs"
         self._redis.rpush(inputs_key, str(args))
-        result =  method(self, *args, **kwargs)
+        result = method(self, *args, **kwargs)
         self._redis.rpush(outputs_key, str(result))
         return result
     return wrapper
+
+
+def replay(method: Callable) -> None:
+    """
+    Replays the method and returns count of calls and history of calls
+    """
+    r = method.__self__._redis
+    name = method.__qualname__
+    count = r.get(name)
+    count_int = int(count.decode("utf-8")) if count else 0
+
+    inputs = r.lrange(f"{name}:inputs", 0, -1)
+    outputs = r.lrange(f"{name}:outputs", 0, -1)
+
+    for inp, out in zip(inputs, outputs):
+        print(f"{name}(*{inp.decode('utf-8')} -> {out.decode('utf-8')})")
+
 
 class Cache:
     """
     A class with __init__ method and a private variable
     """
+
     def __init__(self) -> None:
         """
         Method that initializes redis instance and flushes the db
         """
         import redis
-        
+
         self._redis = redis.Redis()
         self._redis.flushdb()
 
@@ -61,14 +80,16 @@ class Cache:
         self._redis.set(key, data)
 
         return key
-    
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[int, bytes, float, str, None]:
+
+    def get(
+            self, key: str, fn: Optional[Callable] = None
+            ) -> Union[int, bytes, float, str, None]:
         """
         Retrieves data from redis and applies optional conversion
         """
         data = self._redis.get(key)
         if key is not None and fn is not None:
-            return fn(data) 
+            return fn(data)
         return data
 
     def get_str(self, key: str) -> str:
@@ -84,18 +105,3 @@ class Cache:
         """
         key = self.get(key, int)
         return key
-
-    def replay(method: Callable) -> None:
-        """
-        Replays the method and returns count of calls and history of calls
-        """
-        r = method.__self__._redis
-        name = method.__qualname__
-        count = r.get(name)
-        count_int = int(count.decode("utf-8")) if count else 0
-
-        inputs = r.lrange(f"{name}:inputs", 0, -1)
-        outputs = r.lrange(f"{name}:outputs", 0, -1)
-
-        for inp, out in zip(inputs, outputs):
-            print(f"{name}(*{inp.decode('utf-8')} -> {out.decode('utf-8')})")
